a8081ca5d252eb2bb295a19dcd62d38a56a850b7
Martin Vician <martin.vician@nic.cz>
2016-06-13 13:45:14 +0200

    draft for CRo audioarchiv

---
+++ cro.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ARCHIV="$1"
+
+ARCHIV="$(echo $ARCHIV | awk -F'#' '{print $1}')"
+ARCHIV="$(echo $ARCHIV | awk -F'?' '{print $1}')"
+
+echo "cut url: $ARCHIV"
+
+IDs=$(echo "$ARCHIV/" | grep -o '/[0-9]*/$')
+echo $IDs
+ID=$(echo "$IDs" | grep -o '[0-9]*')
+echo $ID
+
+download_url="http://media.rozhlas.cz/_download/$ID"
+stream_url="http://media.rozhlas.cz/_audio/$ID.mp3"
+
+echo "download_url: $download_url"
+echo "download_url: $stream_url"
+
+#mplayer "$download_url"
+mplayer "$stream_url"
