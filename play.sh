aa6ed4dc5b382aa8ac639772b50354dc29ee1c26
Martin Vician <info@vician.cz>
2016-06-13 08:21:44 +0200

    new station

--- play.sh
+++ play.sh
@@ -3,6 +3,13 @@
 dirname=$(dirname $0)
 
 INDEX_FILE="$dirname/index.txt"
+if [ ! -f $INDEX_FILE ] ; then
+	echo "1" > $INDEX_FILE
+	if [ $? -ne 0 ]; then
+		echo "ERROR: Cannot create $INDEX_FILE"
+		exit 1
+	fi
+fi
 RADIO_FILE="$dirname/radio.txt"
 
 INDEX=$(cat $INDEX_FILE)
