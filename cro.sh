772b95d1b8aa1673f74d734729edb905afc1ef08
Martin Vician <martin.vician@nic.cz>
2016-06-14 10:08:52 +0200

    changed descr

--- cro.sh
+++ cro.sh
@@ -16,7 +16,7 @@ download_url="http://media.rozhlas.cz/_download/$ID"
 stream_url="http://media.rozhlas.cz/_audio/$ID.mp3"
 
 echo "download_url: $download_url"
-echo "download_url: $stream_url"
+echo "stream_url: $stream_url"
 
 #mplayer "$download_url"
 mplayer "$stream_url"
